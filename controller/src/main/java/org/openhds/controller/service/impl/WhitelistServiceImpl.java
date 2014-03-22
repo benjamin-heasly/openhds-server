b456bf766827aab9a9b8d38b57ff46a074501d9b
Jeff Faulkner <jeff.faulkner@gmail.com>
2014-03-22 18:38:13 -0400

    Revert "adding angularjs resources"
    This reverts commit cba5895a9f39ab43170977be09b24bc5b354dc27.

--- controller/src/main/java/org/openhds/controller/service/impl/WhitelistServiceImpl.java
+++ controller/src/main/java/org/openhds/controller/service/impl/WhitelistServiceImpl.java
@@ -3,13 +3,9 @@ package org.openhds.controller.service.impl;
 import org.openhds.controller.service.WhitelistService;
 import org.openhds.dao.service.GenericDao;
 import org.openhds.domain.model.Whitelist;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class WhitelistServiceImpl implements WhitelistService {
 
-	private final static Logger log = LoggerFactory.getLogger(WhitelistServiceImpl.class);
-	
 	private GenericDao genericDao;
 
 	public WhitelistServiceImpl(GenericDao genericDao) {
@@ -18,11 +14,10 @@ public class WhitelistServiceImpl implements WhitelistService {
 
 	@Override
 	public boolean isHostIpAddressWhitelisted(String hostIpAddress) {
-		// TODO: enable white listed ip addresses again
-//		Whitelist whitelistRecord = genericDao.findByProperty(Whitelist.class, "address", hostIpAddress);
-//		if (whitelistRecord == null) {
-//			return false;
-//		}
+		Whitelist whitelistRecord = genericDao.findByProperty(Whitelist.class, "address", hostIpAddress);
+		if (whitelistRecord == null) {
+			return false;
+		}
 
 		return true;
 	}
